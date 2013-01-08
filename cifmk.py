c2f9baafc00982cae918d9d7515de3a42739e241
Joel Wilson <elbinray@gmail.com>
2013-01-07 21:30:42 -0800

    added to __main__ for heroku

--- cifmk.py
+++ cifmk.py
@@ -34,5 +34,6 @@ def weather_from_form():
                             zipcode=request.args.get('location')))
 
     
-# if __name__ == '__main__':
-    # app.run(host='0.0.0.0', debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
