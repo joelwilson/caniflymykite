9338a0c0e0e17884d0be567f73ade6b3c1a5c62b
Joel Wilson <elbinray@gmail.com>
2013-01-10 19:07:10 -0800

    Code cleaned up with help from pep8 & pylint

--- cifmk.py
+++ cifmk.py
@@ -1,6 +1,5 @@
 import os
-from flask import Flask, render_template, abort, request, url_for, redirect, \
-    flash
+from flask import Flask, render_template, abort, request, url_for, redirect
 
 from noaa import get_weather, tomph, heading, WeatherError, iszip
 
@@ -10,29 +9,33 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/zip/<zipcode>')
 def get_by_zip(zipcode=95382):
+    '''Returns a page for a specific zip code, displaying wind speed, etc.'''
     if not iszip(zipcode):
         print "not a valid zip! %s" % zipcode
         return render_template('error.html', msg="Invalid zip code")
     try:
-        w = get_weather(zipcode)
+        weather = get_weather(zipcode)
     except WeatherError:
         abort(404)
     args = {
-        'wind_speed': tomph(w.val('wind_speed')),
-        'wind_dir': heading(w.val('wind_dir')),
-        'rain_chance': w.val('rain_prob'),
+        'wind_speed': tomph(weather.val('wind_speed')),
+        'wind_dir': heading(weather.val('wind_dir')),
+        'rain_chance': weather.val('rain_prob'),
         'location': zipcode,
-        'current_temp': w.val('temperature')
+        'current_temp': weather.val('temperature')
     }
     return render_template('index.html', **args)
 
 
 @app.route('/get_weather/', methods=['GET'])
 def weather_from_form():
-    return redirect(url_for('get_by_zip', 
+    '''Extracts the user provided zip code from the form data and redirects
+    to the appropriate zip code page.
+    '''
+    return redirect(url_for('get_by_zip',
                             zipcode=request.args.get('location')))
 
-    
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
--- noaa.py
+++ noaa.py
@@ -10,7 +10,7 @@ import requests
 __all__ = ['Weather', 'get_weather', 'query_noaa', 'WeatherError', 'between',
            'parse_xml', 'isvalid', 'tomph', 'NOAA_ELEMS', 'heading', 'iszip']
 URL = 'http://graphical.weather.gov/xml/sample_products/browser_interface/ndfdXMLclient.php'
-NOAA_ELEMS = ['temp', 'qpf', 'snow', 'pop12', 'sky', 'wdir', 'wspd', 'wgust']
+NOAA_ELEMS = ('temp', 'qpf', 'snow', 'pop12', 'sky', 'wdir', 'wspd', 'wgust')
 XML_WEATHER_MAP = {
     ('precipitation', 'liquid'):                    'rain',
     ('precipitation', 'snow'):                      'snow',
@@ -27,23 +27,24 @@ XML_WEATHER_MAP = {
 
 
 class WeatherError(Exception):
+    '''Basic exception class for exceptions in the Weather class.'''
     pass
 
 
 class Weather(object):
     '''Container class for storing and retrieving weather data.
-    Input is the XML response text retrieved from an NOAA query. Weather 
+    Input is the XML response text retrieved from an NOAA query. Weather
     elements are accessed by name using the val() function.
     '''
     def __init__(self, xml):
         if not isvalid(xml):
-            raise WeatherError, 'Invalid XML data: \n{0}'.format(xml)
+            raise WeatherError('Invalid XML data: \n{0}'.format(xml))
         else:
             parsed = parse_xml(xml)
         self.latlon = (parsed['latitude'], parsed['longitude'])
         self.times = parsed['times']
         self.weather = parsed['weather']
-    
+
     def val(self, element, when=datetime.utcnow()):
         '''Given a weather element type and optional datetime object,
         returns the weather value closest to when.'''
@@ -51,73 +52,75 @@ class Weather(object):
         closest = min(times, key=lambda t: abs((when - t)).total_seconds())
         index = times.index(closest)
         return self.weather[element]['values'][index]
-        
+
     def __repr__(self):
         items = {
             'name': self.__class__.__name__,
-            'latlon': self.latlon, 
+            'latlon': self.latlon,
             'times': self.times.keys(),
             'wx': self.weather.keys()
         }
         return "{name}({latlon}, {times}, {wx})".format(**items)
 
 
-def get_weather(zip, elems=NOAA_ELEMS):
+def get_weather(zipcode, elems=NOAA_ELEMS):
     '''Returns weather data for a zip code as an instance of a weather
     data class.'''
-    xml = query_noaa(zip, elems)
+    xml = query_noaa(zipcode, elems)
     return Weather(xml)
 
 
-def query_noaa(zip, elems, url=URL):
+def query_noaa(zipcode, elems, url=URL):
+    '''Returns the XML text returned by a query to NOAA with the passed
+    zipcode and list or tuple of weather elements, elems.'''
     params = {
-        'zipCodeList': zip,
+        'zipCodeList': zipcode,
         'product': 'time-series'
     }
-    params.update((e, e) for e in elems)
-    r = requests.get(URL, params=params)
+    params.update((elem, elem) for elem in elems)
+    r = requests.get(url, params=params)
     return r.text if r.ok else None
 
-    
+
 def parse_xml(xml):
     '''Parses the XML structure. Returns dict of weather, time series,
-    latitude, and longitude values and attributes. Works with the results 
+    latitude, and longitude values and attributes. Works with the results
     from a single point only.
     '''
-    r = {'times': {}, 'weather': {}}
+    results = {'times': {}, 'weather': {}}
     root = ET.fromstring(xml)
     # lat & lon
-    r.update(i for p in root.findall('./data/location/point') 
-             for i in p.items())
+    results.update(i for points in root.findall('./data/location/point')
+                   for i in points.items())
     # times by layout-key
-    for n in root.findall('./data/time-layout/'):
-        if n.tag == 'layout-key':
-            key = n.text
-            r['times'][key] = []
-        if n.tag == 'start-valid-time':
-            r['times'][key].append(datetime.strptime(n.text[0:19], 
+    for node in root.findall('./data/time-layout/'):
+        if node.tag == 'layout-key':
+            key = node.text
+            results['times'][key] = []
+        if node.tag == 'start-valid-time':
+            results['times'][key].append(datetime.strptime(node.text[0:19],
                 '%Y-%m-%dT%H:%M:%S'))
     # weather parameters
-    for p in root.findall('./data/parameters/*'):
-        key = XML_WEATHER_MAP[(p.tag, p.get('type'))]
-        r['weather'][key] = defaultdict(list)
-        r['weather'][key]['time-layout'] = (p.get('time-layout'))
-        for v in p.findall('./value'):
-            r['weather'][key]['values'].append(v.text)
-    return r
-    
+    for param in root.findall('./data/parameters/*'):
+        key = XML_WEATHER_MAP[(param.tag, param.get('type'))]
+        results['weather'][key] = defaultdict(list)
+        results['weather'][key]['time-layout'] = (param.get('time-layout'))
+        for value in param.findall('./value'):
+            results['weather'][key]['values'].append(value.text)
+    return results
+
 
 def isvalid(xml):
     '''Returns True if the xml response is NOAA valid.'''
     errors = ['<h2>ERROR</h2>', '<errorMessage>', '<error>']
-    for e in errors: 
-        if e in xml:
+    for error in errors:
+        if error in xml:
             return False
     if 'http://www.w3.org/' not in xml:
         return False
     return True
-    
-    
+
+
 def iszip(zipcode):
     '''Returns True is the provided zip code is valid.'''
     xml = query_noaa(zipcode, ['sky'])
@@ -125,21 +128,20 @@ def iszip(zipcode):
         return True
     else:
         return False
-    
-    
+
+
 def tomph(knots, precision=2):
     '''Converts knots to MPH.'''
     return round(float(knots) * 1.15078, precision)
 
-    
+
 def heading(deg):
     '''Returns a string representation of an numerical direction in degrees.
-    
+
     Examples:
         heading(360) => "N"
         heading('45')  => "NE"
     '''
-    tick = 22.5
     deg = float(deg)
     head = {
         'N1':  (0,     22.5),
@@ -156,8 +158,8 @@ def heading(deg):
         if between(deg, *val):
             return 'N' if key == 'N1' or key == 'N2' else key
     return None
-    
-    
-def between(n, low, hi):
-    '''Returns True if n is between low and hi (inclusive), else False.'''
-    return True if n >= low and n <= hi else False
+
+
+def between(num, low, high):
+    '''Returns True if num between low & high (inclusive), else False.'''
+    return True if num >= low and num <= high else False
