2a3ba1bb54a7cb69954bfb45169b61513d648366
Joel Wilson <elbinray@gmail.com>
2013-03-27 21:51:48 -0700

    Modified kites.html to display the new text attribute descriptions, added an into paragraph, and laid out a possible location for the legend.

--- kites.py
+++ kites.py
@@ -4,26 +4,13 @@ from collections import defaultdict
 
 class Kite(dict):
     '''Class containing kites information.'''
-    def __init__(self, *arg, **kwargs):
-        super(Kite, self).__init__(*arg, **kwargs)
-        #self['difficulty'] = (u'\u2726' * int(self['difficulty']))
-        self._make_ratings()
-        
-    def _make_ratings(self):
-        '''Converts the numerical ratings into visual strings.'''
-        try:
-            self['difficulty'] = self._rating_string('difficulty')
-            self['power'] = self._rating_string('power')
-            self['speed'] = self._rating_string('speed')
-            self['price'] = self._rating_string('price')
-        except ValueError:
-            pass
-
-    def _rating_string(self, key, tick=u'\u2726', notick=u'\u2727'):
-        '''Returns a string containing 5 characters representing the
-        rating.'''
-        num_ticks = int(self[key])
-        return (tick * num_ticks + notick * (5 - num_ticks))
+    def __init__(self, **kwargs):
+        unicode_kwargs = kwargs
+        for key, item in kwargs.iteritems():
+            unicode_kwargs[key] = item.decode('utf-8')
+        # call the dict objs init method, letting it do the dict setup magic
+        super(Kite, self).__init__(**unicode_kwargs)
+        # u'\u2727'
 
 
 def get_kites(csvfile='./static/kites.csv'):
@@ -32,5 +19,5 @@ def get_kites(csvfile='./static/kites.csv'):
     with open(csvfile, 'r') as kite_csv:
         kitereader = csv.DictReader(kite_csv)
         for row in kitereader:
-            kites[row['category']].append(Kite(row))
+            kites[row['category']].append(Kite(**row))
     return kites
