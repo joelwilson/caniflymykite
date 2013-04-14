d8891f2d8688fbc9070340a10e036351cb80d824
Joel Wilson <elbinray@gmail.com>
2013-04-13 17:30:26 -0700

    Finished up basic wunderground functionality.

--- wunderground.py
+++ wunderground.py
@@ -1,25 +1,39 @@
 import json
+import os
+
+import requests
 
 
+API_KEY = os.environ['WUNDERGROUND_KEY']
 
-def query_wunderground(api_key, features, query=False, file_format="JSON"):
+
+def query_wunderground(api_key, features, query, file_format="json"):
     '''Generic API query function for making any query to the Wunderground
     API and returning the JSON result. Probably re-inventing the wheel, 
-    but oh, well.'''
+    but oh well.'''
     baseurl = 'http://api.wunderground.com/api/'
-    r = requests.get(baseurl + '{key}/{features}/q/{query}.{format}'.format(
-                     {'key': api_key, 'features': '/'.join(features), 
-                         'query': query.strip(), 'format': file_format}
+    r = requests.get(
+        baseurl + '{key}/{features}/q/{query}.{format}'.format(
+            key=api_key,
+            features='/'.join(features),
+            query=str(query).strip(),
+            format=file_format
+        )
+    )
     return json.loads(r.text)
 
 
-def current_conditions_search(query):
+def current_conditions(query):
     '''Searches Wunderground for a location matching the given query. 
     Returns a dict of results as returned from Wunderground.'''
-    return query_wunderground(API_KEY, ['conditions'], query=query)
+    return query_wunderground(API_KEY, ['conditions'], query)
 
 
-def three_day_forecast(query):
+def forecast(query):
     '''Searches Wunderground for a location matching the given query and
     returns the 3 day forecast for the location.'''
-    pass
+    return query_wunderground(API_KEY, ['forecast'], query)
+
+if __name__ == '__main__':
+    from pprint import pprint
+    pprint(current_conditions('springfield'))
